bba097043e367c935c5adec9a9cf5d7384e88bcb
Eder Marques <eder.marques@citi.org.br>
2018-07-29 11:24:27 -0300

    fix wsgi

--- Integrar/wsgi.py
+++ Integrar/wsgi.py
@@ -10,6 +10,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Integrar.settings")
 
 application = Cling(get_wsgi_application())
